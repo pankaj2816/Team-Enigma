0407237d268c4d28cfce5b9b833ea9414b7061d8
Pankaj Gupta <pppankaj2816@gmail.com>
2019-03-10 14:19:27 +0530

    Add files via upload

---
+++ earthquake_and_landslide_Hack_on_hills.ino
@@ -0,0 +1,94 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+float a,b,c,d,e,f;
+
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+
+#define OUTPUT_READABLE_ACCELGYRO
+
+
+
+
+int sensor_pin = 2;
+
+int output_value;
+
+
+
+void setup() {
+
+
+   Serial.begin(115200);
+
+   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+
+   Serial.println("Reading From the Sensor ...");
+
+   }
+
+
+
+void loop() {
+  
+   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+//        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.print(gz); Serial.print("\t");
+    #endif
+a=ax;
+b=ay;
+c=az;
+d=gx;
+e=gy;
+f=gz;
+    output_value = analogRead(sensor_pin);
+
+    Serial.println(output_value);
+   if(a>2500 || a<-2500 || b>2500 || b<-2500)     //landslide
+    {
+      digitalWrite(13, 1);
+      delay(1000);
+    }
+    if(d>400 || d<-400 || e>150 || e<-150 ||f>150||f<-150) //Earthquake
+    {
+       digitalWrite(12, 1);
+       delay(1000);
+    }
+     digitalWrite(12, 0);
+    digitalWrite(13, 0);
+//    delay (1);
+
+   }
---
+++ enigma1.ino
@@ -0,0 +1,91 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#define WLAN_SSID       "enigma"
+#define WLAN_PASS       "enigma12"
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "anshul2498"
+#define AIO_KEY         "0cdb29698e494b9d86ce0129b13ee767"
+int earthquake,landslide;
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish onn = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ax");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Publish off = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/bx");
+void MQTT_connect();
+
+void setup() {
+  pinMode(13,INPUT);
+  pinMode(12,INPUT);
+    Serial.begin(9600);
+
+  Serial.println(WLAN_SSID);
+  Serial.print("Connecting to ");
+  
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+MQTT_connect();
+
+   }
+uint32_t x=0;
+
+
+void loop() {
+earthquake=digitalRead(13);
+ if(earthquake== HIGH)
+    {    
+  onn.publish(1);
+  Serial.println("earthquake");
+  delay(1000);
+   }
+   landslide= digitalRead(12);
+   if(landslide== HIGH)
+    {    
+  off.publish(1);
+  Serial.println("landslide");
+  delay(1000);
+   }
+}
+
+   void MQTT_connect() {
+  int8_t ret;
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+   }
